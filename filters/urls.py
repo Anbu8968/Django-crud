1c96b414eb8c4f898980abbe74d133a6cf323a2e
Anbu <anbu2872001@gmail.com>
2023-06-06 19:35:03 +0530

    hands on orm filters

--- filters/urls.py
+++ filters/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.apiOverview,name="api overview")
+    path("",views.apiOverview,name="api overview"),
+    path('age/',views.filter_by_awh,name="filter_by_age"),
+    path('get-names/',views.get_names),
+    path('create/',views.create_object),
+    path('update/',views.update_person),
+    path('values-col/',views.values_by_col)
 ]
--- filters/views.py
+++ filters/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def apiOverview(request):
     api_urls={
-        "filter":"filter/",
+        "filter":"filter-age/",
         "values":"values/",
         "value_list":"value-list/",
         "get":"get/",
@@ -20,4 +20,51 @@ def apiOverview(request):
     return Response(api_urls)
 
 
+@api_view(['GET'])
+def filter_by_awh(request):
+    age=request.data['age']
+    weight=request.data['weight']
+    height=request.data['height']
+    person=Person.objects.filter(age__gte=age,weight__gte=weight,height__gte=height)
+    return Response(person)
+
+@api_view(['GET'])
+def values_by_col(request):
+    col_name=request.data['col_names']
+    col_values=Person.objects.values(col_name)
+    if col_values:
+        return Response(col_values)
+    else:
+        return Response("no data")
+
+@api_view(['GET'])
+def get_names(request):
+    name=request.data['name']
+    person=Person.objects.get(name=name)
+    if person:
+        return Response(person)
+    else:
+        return Response("No data")
+
+
+@api_view(['GET'])
+def create_object(request):
+    name=request.data['name']
+    age=request.data['age']
+    weight=request.data['weight']
+    height=request.data['height']
+    person=Person.objects.create(name=name,age=age,weight=weight,height=height)
+    return Response("Created")
+
+@api_view(['GET'])
+def update_person(request):
+    name=request.data['name']
+    age=request.data['age']
+    weight=request.data['weight']
+    height=request.data['height']
+    person=Person.objects.filter(name=name).update(age=age,weight=weight,height=height)
+    if person:
+        return Response("Updated")
+    else:
+        return Response("no data find")
 
